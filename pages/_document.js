cbdac8d7f46fb09a4152dd758ff04a26dd719f99
najmi9 <imadnajmi9@gmail.com>
2022-02-25 09:57:56 +0100

    Import bootstrap & main.css

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,12 @@ export default function Document() {
         <link href="/favicon.png" rel="icon" />
         <link href="/img/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i?display=optional" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
       </body>
     </Html>
   )
